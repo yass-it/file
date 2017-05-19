de4aac71ff18da7443293df24130f5c1d31f0c19
Administrateur <administrateur@MacBook-Air-de-MacUser.local>
2017-05-19 15:35:02 +0200

    1

---
+++ operations object.js
@@ -0,0 +1,25 @@
+
+var tempateOperation = {
+name : '',
+args : 0000,
+operation: funaction() {};
+};
+var name = 'june'
+
+function dynamiCalculator(op,a,b,c){
+var result = 00000;
+// code goes here 
+return result;
+	
+};
+
+
+
+var addtion = {
+name : 'addition',
+args : 0000,
+operation: funaction(a,b) {
+	var result = 00000;
+
+	return result ;
+};
